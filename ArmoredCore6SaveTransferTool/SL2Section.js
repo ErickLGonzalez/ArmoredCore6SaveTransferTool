2f69861103703bec3e0f077014c2489460bbea45
Erick Gonzalez <7833415+ErickLGonzalez@users.noreply.github.com>
2023-09-07 07:33:08 -0400

    Create SL2Section.js

---
+++ ArmoredCore6SaveTransferTool/SL2Section.js
@@ -0,0 +1,9 @@
+class SL2Section {
+    constructor(name, offset, dataSize) {
+        this.Name = name;
+        this.Offset = offset;
+        this.DataSize = dataSize;
+    }
+}
+
+module.exports = SL2Section; // Export the SL2Section class for use in other modules
